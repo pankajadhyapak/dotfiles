9a4c3039b25365ab0f1f4ea6f8acdff023a3e8f4
Pankaj Adhyapak <pad@hellofresh.com>
2020-11-19 18:25:49 +0100

    First draft

---
+++ Brewfile
@@ -0,0 +1,67 @@
+# Taps
+tap 'homebrew/cask'
+tap 'homebrew/cask-versions'
+tap 'homebrew/bundle'
+tap 'blendle/blendle'
+
+# Binaries
+brew 'awscli'
+brew 'coreutils' # Those that come with macOS are outdated
+brew 'git'
+brew 'grep'
+brew 'httpie'
+brew 'trash' # Manage the Trash bin
+brew 'tree' # List directories in a tree structure
+
+
+
+# Development
+brew 'php'
+brew 'php@7.3'
+brew 'composer'
+brew 'imagemagick'
+brew 'libmemcached'
+brew 'memcached'
+brew 'node'
+brew 'yarn'
+brew 'vault'
+brew 'kns'
+brew 'stern'
+brew 'helm'
+brew 'go'
+brew 'gitleaks'
+brew 'terraform'
+brew 'helm@2'
+brew 'youtube-dl'
+
+# Apps
+cask 'caffeine'
+cask 'iterm2'
+cask 'docker'
+cask 'github'
+cask 'clipy'
+cask 'google-backup-and-sync'
+cask 'google-chrome'
+cask 'postman'
+cask 'notion'
+cask 'phpstorm'
+cask 'goland'
+cask 'skype'
+cask 'slack'
+cask 'evernote'
+cask 'dropbox'
+cask 'sublime-text'
+cask 'tableplus'
+cask 'telegram-desktop'
+cask 'the-unarchiver'
+cask 'there'
+cask 'vlc'
+cask 'whatsapp'
+cask 'appcleaner'
+cask 'openvpn-connect'
+
+# Quicklook
+cask 'qlcolorcode'
+cask 'qlmarkdown'
+cask 'quicklook-json'
+cask 'quicklook-csv'
---
+++ fresh.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
+
+# Set default MySQL root password and auth type.
+# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+
+# # Install PHP extensions with PECL
+# pecl install memcached imagick
+
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer laravel/valet
+
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir $HOME/Documents/sites
+mkdir $HOME/Documents/code
+mkdir $HOME/Documents/gocode
+mkdir $HOME/screenshots
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
